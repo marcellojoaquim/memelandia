448048093e541afa4d61f3422c9e5e9eb8ec1d7c
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2026-04-18 11:39:51 -0300

    adicionando os servidores de descoberta de serviços e de configuração

---
+++ config-service/src/test/java/com/marcello/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcello.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
