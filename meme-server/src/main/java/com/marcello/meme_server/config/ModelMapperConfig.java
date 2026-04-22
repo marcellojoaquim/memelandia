e57ffe27200df3ebf0c6f5c0ffe7fd1c393d49c2
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2026-04-22 20:20:31 -0300

    adicionando dependencias necessarias

---
+++ meme-server/src/main/java/com/marcello/meme_server/config/ModelMapperConfig.java
@@ -0,0 +1,7 @@
+package com.marcello.meme_server.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+}
--- meme-server/src/main/java/com/marcello/meme_server/config/SwaggerConfig.java
+++ meme-server/src/main/java/com/marcello/meme_server/config/SwaggerConfig.java
@@ -1,7 +1,27 @@
 package com.marcello.meme_server.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Microsservico Meme")
+                        .description("Microsservico destinado a gerenciamento dos memes que serão postados e exibidos no feed")
+                        .contact(new Contact()
+                                .name("Marcello J Silva")
+                                .email("marcellojoaquim1@hotmail.com")
+                                .url("https://github.com/marcellojoaquim"))
+                        .termsOfService("")
+                        .license(new License()
+                                .name("Apache 2.0")));
+    }
 }
