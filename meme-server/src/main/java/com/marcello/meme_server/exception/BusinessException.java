be56955ce919641d6c031c5a5e834e8f769cff45
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2026-04-22 20:56:59 -0300

    implementando apierrors

---
+++ meme-server/src/main/java/com/marcello/meme_server/exception/BusinessException.java
@@ -0,0 +1,4 @@
+package com.marcello.meme_server.exception;
+
+public class BusinessException {
+}
--- meme-server/src/main/java/com/marcello/meme_server/exception/errors/ApiErrors.java
+++ meme-server/src/main/java/com/marcello/meme_server/exception/errors/ApiErrors.java
@@ -1,4 +1,25 @@
 package com.marcello.meme_server.exception.errors;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ApiErrors {
+    List<String> errors;
+
+    public ApiErrors(BindingResult bindingResult) {
+        this.errors = new ArrayList<>();
+        bindingResult.getAllErrors().forEach(err -> this.errors.add(err.getDefaultMessage()));
+    }
+
+    public ApiErrors(ResponseStatusException exception) {
+        this.errors = Arrays.asList(exception.getReason());
+    }
+
+    public ApiErrors(BusinessException businessException) {
+        this.errors = Arrays.asList(businessException.getMessage());
+    }
 }
