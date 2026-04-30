82480135b2ed1b368e70fc36a9f12e1a33576948
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2026-04-29 22:03:35 -0300

    Implementado o mapeamento de rotas no gateway

--- gateway-service/src/main/java/com/marcello/gateway_service/routing/FeedRoutingConfig.java
+++ gateway-service/src/main/java/com/marcello/gateway_service/routing/FeedRoutingConfig.java
@@ -1,9 +1,38 @@
 package com.marcello.gateway_service.routing;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeedRoutingConfig {
 
     private final String SERVER_URI = "http://localhost:8082";
+
+    @Bean
+    public RouteLocator feedRoutesLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder
+                .routes()
+                .route("buscar-feed", p ->
+                        p.path("/feed")
+                                .uri(SERVER_URI))
+                .route("buscar-categorias", p ->
+                        p.path("/feed/categorias")
+                                .and()
+                                .uri(SERVER_URI))
+                .route("buscar-memes-por-categoria", p ->
+                        p.path("/feed/categoria/{categoria}")
+                                .and()
+                                .uri(SERVER_URI))
+                .route("buscar-memes-por-usuario", p ->
+                        p.path("/feed/usuario/{email}")
+                                .and()
+                                .uri(SERVER_URI))
+                .route("buscar-meme-do-dia", p ->
+                        p.path("/feed/meme-do-dia")
+                                .and()
+                                .uri(SERVER_URI))
+                .build();
+    }
 }
--- gateway-service/src/main/java/com/marcello/gateway_service/routing/PostRoutingConfig.java
+++ gateway-service/src/main/java/com/marcello/gateway_service/routing/PostRoutingConfig.java
@@ -1,9 +1,25 @@
 package com.marcello.gateway_service.routing;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class PostRoutingConfig {
 
     private final String SERVER_URI = "http://localhost:8083";
+
+    @Bean
+    public RouteLocator postRoutesLocator(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder
+                .routes()
+                .route("Criar-post", p ->
+                        p.path("/posts")
+                                .and()
+                                .method(HttpMethod.POST)
+                                .uri(SERVER_URI))
+                .build();
+    }
 }
